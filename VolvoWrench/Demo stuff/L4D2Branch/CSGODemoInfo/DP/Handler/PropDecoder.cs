3bd51fbf69135d971da0c977047e64b878ce6540
Jukspa <jukspaz@gmail.com>
2017-08-30 14:15:48 +0300

    Update PropDecoder.cs

--- VolvoWrench/Demo stuff/L4D2Branch/CSGODemoInfo/DP/Handler/PropDecoder.cs
+++ VolvoWrench/Demo stuff/L4D2Branch/CSGODemoInfo/DP/Handler/PropDecoder.cs
@@ -288,11 +288,11 @@ namespace VolvoWrench.Demo_Stuff.L4D2Branch.CSGODemoInfo.DP.Handler
                     // Adjust the integers from [0..MAX_COORD_VALUE-1] to [1..MAX_COORD_VALUE]
                     if (inBounds)
                     {
-                        intval = reader.ReadInt(11) + 1;
+                        intval = (int)reader.ReadInt(11) + 1;
                     }
                     else
                     {
-                        intval = reader.ReadInt(14) + 1;
+                        intval = (int)reader.ReadInt(14) + 1;
                     }
                 }
 
